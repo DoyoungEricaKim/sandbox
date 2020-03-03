6f655a6b1c640a9a548dcadcc04aab5861d39c94
juli0327@naver.com <a105030336!>
2020-03-03 17:08:27 +0900

    update baseball game with functions

--- hw0304_plus.py
+++ hw0304_plus.py
@@ -1,37 +1,60 @@
-# baseball game
 
 import random
 
-answer = str(random.randint(100, 999))
-count = 0
+
+def pick_quiz():
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    a = nums[random.randint(0, len(nums) - 1)]
+    nums.remove(a)
+    b = nums[random.randint(0, len(nums) - 1)]
+    nums.remove(b)
+    c = nums[random.randint(0, len(nums) - 1)]
+    quiz = str(a) + str(b) + str(c)
+    print(quiz)
+    return quiz
 
 
-def baseball(a, b):
+def validate(num):
+    if "0" in num:
+        print("Error: Only Three-digit-number without zero is allowed.")
+        return False
+    else:
+        return True
+
+
+def determine(quiz, guess):
     strike = 0
     ball = 0
-    for i in range(0, len(answer)):
-        for j in range(0, len(guess)):
-            if answer[i] == guess[j]:  # 숫자 같음
-                if i == j:  # 자리도 같으면 strike
-                    strike += 1
-                else:       # 자리는 다르면 ball
-                    ball += 1
-    print(str(strike) + " Strike, " + str(ball) + " ball")
+    if validate(guess):
+        for i in range(0, len(quiz)):
+            for j in range(0, len(guess)):
+                if quiz[i] == guess[j]:  # 숫자 같음
+                    if i == j:  # 자리도 같으면 strike
+                        strike += 1
+                    else:  # 자리는 다르면 ball
+                        ball += 1
+        print(str(strike) + " Strike, " + str(ball) + " ball")
 
 
-count += 1
-print(answer)
-guess = input("# " + str(count) + ": Your guess? ")
+def playball(quiz):
+    count = 0
+    count += 1
 
-if answer == guess:
-    pass
-else:
-    baseball(answer, guess)
-    while True:
-        count += 1
-        guess = input("# " + str(count) + ": Your guess? ")
-        if answer == guess:
-            break
-        else:
-            baseball(answer, guess)
-print("Homerun!!!!")
+    print("quiz:" + quiz)
+    guess = input("# " + str(count) + ": Your guess? ")
+    if quiz == guess:
+        pass
+    else:
+        determine(quiz, guess)
+        while True:
+            count += 1
+            guess = input("# " + str(count) + ": Your guess? ")
+            if quiz == guess:
+                break
+            else:
+                determine(quiz, guess)
+    print("Homerun!!!!")
+
+
+if __name__ == '__main__':
+    playball(pick_quiz())
